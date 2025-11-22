fdd43572858e55f0dfa839ad8a4e1463f1798ab4
Martin Marek <martin.marek@email.cz>
2025-11-22 11:38:43 +0100

    helloworld add useState

--- helloworld/src/HelloWorld.jsx
+++ helloworld/src/HelloWorld.jsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
+
 // function HelloWorld(props) {
-function HelloWorld({text, name}) {
+function HelloWorld({ text, name }) {
 
     //const text = props.text;
     //const name = props.name;
-    
+
     //const {text, name} = props;
 
-    console.log("Text: " + text + ", Name: " +  name);
+    console.log("Text: " + text + ", Name: " + name);
 
-    return <h1>{text} světe! {name}</h1>
+    const [meno, setMeno] = useState("Používatel");
+
+    function handleMenoChange(event) {
+        setMeno(event.target.value);
+    }
+
+    return (
+        <div>
+            <h1>{text} světe! {meno}</h1>
+            <input type="text" value={meno} onChange={handleMenoChange} />
+        </div>
+    )
+
+    //return <h1>{text} světe! {name}</h1>
 }
 
 export default HelloWorld;
